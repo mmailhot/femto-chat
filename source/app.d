605158e6ca993c4b3fc1979f1b99358da4698461
Marc Mailhot <mmm11105@gmail.com>
2016-01-21 04:45:39 +0000

    Can now connect over TCP and print the initial headers from SynIRC

--- source/app.d
+++ source/app.d
@@ -1,6 +1,16 @@
+module femtochat.app;
+
 import std.stdio;
+import std.concurrency;
+import femtochat.connection;
+import femtochat.messages;
+import core.thread;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+  Tid connTid = spawn(&spawnConnection, thisTid, "irc.synirc.net", cast(ushort)6667);
+  writeln("Waiting");
+  Thread.sleep( dur!"seconds"(5) );
+  writeln("Killing");
+  send(connTid, MSG_KILL());
 }
